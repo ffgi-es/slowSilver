4a6e5015036d3a7442c8725d49b4b509f2cafa36
Alastair Gilles <agilles91@gmail.com>
2020-07-13 21:25:37 +0200

    feat: clean up after compiling and tests

--- spec/compiling_tests/integer_return/compile_spec.rb
+++ spec/compiling_tests/integer_return/compile_spec.rb
@@ -1,11 +1,22 @@
 describe "compiling and running program" do
   let("slwslvr") { "./slwslvr" }
   let("sample") { File.expand_path("sample.sag", File.dirname(__FILE__)) }
+  let("out_file") { "a.out" }
+
+  after(:example) do
+    File.delete out_file if File.exists? out_file
+  end
 
   it "should return 2" do
     `#{slwslvr} #{sample}`
     expect($?.exitstatus).to be 0
-    `./a.out`
+    `./#{out_file}`
     expect($?.exitstatus).to be 2
+  end
+
+  it "should delete its temporary files" do
+    `#{slwslvr} #{sample}`
+    expect(Dir['*.asm']).to be_empty
+    expect(Dir['*.o']).to be_empty
   end
 end
