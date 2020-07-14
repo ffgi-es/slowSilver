a2a4606850d4f11ad0bb1f51b7c495460a93b9e6
Alastair Gilles <agilles91@gmail.com>
2020-07-14 09:42:25 +0200

    refactor: add simplfied command pattern for lexing

--- lib/Lexer.rb
+++ lib/Lexer.rb
@@ -4,33 +4,44 @@ class Lexer
   def self.lex(filename)
     File.open filename, "r" do |file|
       file.gets
-        .split(/(\s|\.|=>)/)
+        .split(/(\s|\.)/)
         .reject { |s| s =~ /^\s*$/ }
-        .map { |part| lex_type part }
+        .map { |part| lex_part(part, @@lexings) }
+    end
+  end
+
+  def self.lex_part(string, lexings)
+    lexings.each do |lexing|
+      result = lexing.call(string)
+      return result unless result.nil?
     end
   end
 
   def self.lex_type(string)
-    return Token.new(:type, string.to_sym) if string =~ /^[A-Z]+$/
-    lex_id(string)
+    Token.new(:type, string.to_sym) if string =~ /^[A-Z]+$/
   end
 
   def self.lex_id(string)
-    return Token.new(:identifier, string) if string =~ /^[a-z]+$/
-    lex_return(string)
+    Token.new(:identifier, string) if string =~ /^[a-z]+$/
   end
 
   def self.lex_return(string)
-    return Token.new(:return) if string =~ /^=>$/
-    lex_int(string)
+    Token.new(:return) if string =~ /^=>$/
   end
 
   def self.lex_int(string)
-    return Token.new(:integer_constant, string.to_i) if string =~ /^[0-9]+$/
-    lex_end(string)
+    Token.new(:integer_constant, string.to_i) if string =~ /^[0-9]+$/
   end
   
   def self.lex_end(string)
-    return Token.new(:end) if string =~ /^\.$/
+    Token.new(:end) if string =~ /^\.$/
   end
+
+  @@lexings = [
+    method(:lex_type),
+    method(:lex_id),
+    method(:lex_return),
+    method(:lex_int),
+    method(:lex_end)
+  ]
 end
