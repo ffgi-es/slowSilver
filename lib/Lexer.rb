04fec2d62d87e451aec20b9a864815294e7dab0c
Alastair Gilles <agilles91@gmail.com>
2020-07-14 02:23:04 +0200

    refactor: change tokens to symbols

--- lib/Lexer.rb
+++ lib/Lexer.rb
@@ -1,12 +1,34 @@
 class Lexer
   def self.lex(filename)
     File.open filename, "r" do |file|
-      line = file.gets
-      line.split
-        .map { |part| part.gsub(/=>/, 'RETURN') }
-        .map { |part| part.split /(\.)/ }
-        .flatten
-        .map { |part| part.gsub('.', 'END') }
+      file.gets
+        .split(/(\s|\.|=>)/)
+        .reject { |s| s =~ /^\s*$/ }
+        .map { |part| lex_type part }
     end
+  end
+
+  def self.lex_type(string)
+    return "type_#{string}".to_sym if string =~ /^[A-Z]+$/
+    lex_id(string)
+  end
+
+  def self.lex_id(string)
+    return "id_#{string}".to_sym if string =~ /^[a-z]+$/
+    lex_return(string)
+  end
+
+  def self.lex_return(string)
+    return :return if string =~ /^=>$/
+    lex_int(string)
+  end
+
+  def self.lex_int(string)
+    return "cons_int_#{string}".to_sym if string =~ /^[0-9]+$/
+    lex_end(string)
+  end
+  
+  def self.lex_end(string)
+    return :end if string =~ /^\.$/
   end
 end
--- spec/lexer/lexer_spec.rb
+++ spec/lexer/lexer_spec.rb
@@ -4,13 +4,25 @@ describe "Lexer" do
   describe "#lex" do
     it "should return a list of tokens for sample1.sag" do
       in_file = File.expand_path "sample1.sag", File.dirname(__FILE__)
-      out_tokens = ['INT', 'main', 'RETURN', '4', 'END']
+      out_tokens = [
+        :type_INT,
+        :id_main,
+        :return,
+        :cons_int_4,
+        :end
+      ]
       expect(Lexer.lex in_file).to eq out_tokens
     end
 
     it "should return a list of tokens for sample2.sag" do
       in_file = File.expand_path "sample2.sag", File.dirname(__FILE__)
-      out_tokens = ['INT', 'main', 'RETURN', '6', 'END']
+      out_tokens = [
+        :type_INT,
+        :id_main,
+        :return,
+        :cons_int_6,
+        :end
+      ]
       expect(Lexer.lex in_file).to eq out_tokens
     end
   end
