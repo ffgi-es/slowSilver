1e668c889be5226c180635c0b0fdf8b8bcdba525
Alastair Gilles <agilles91@gmail.com>
2020-08-02 01:29:20 +0200

    refactor: extract default actions to their own class

--- lib/ast.rb
+++ lib/ast.rb
@@ -1,4 +1,5 @@
 require_relative 'register'
+require_relative 'actions'
 
 # root of the AST
 class ASTree
@@ -93,63 +94,10 @@ end
 class Expression
   attr_reader :function, :parameters
 
-  @actions = Hash.new(
-    proc do |res, name, params|
-      res << "push #{Register[:ax]}".asm unless params.empty?
-      res << "call _#{name}".asm
-      res << "add #{Register[:sp]}, #{params.count * 8}".asm unless params.empty?
-      res
-    end)
-
-  @actions[:+] = proc do |res|
-    res << "pop #{Register[:cx]}".asm
-    res << "add #{Register[:ax]}, #{Register[:cx]}".asm
-  end
-
-  @actions[:-] = proc do |res|
-    res << "pop #{Register[:cx]}".asm
-    res << "sub #{Register[:ax]}, #{Register[:cx]}".asm
-  end
-
-  @actions[:"="] = proc do |res|
-    res << "mov #{Register[:bx]}, #{Register[:ax]}".asm
-    res << "pop #{Register[:cx]}".asm
-    res << "xor #{Register[:ax]}, #{Register[:ax]}".asm
-    res << "cmp #{Register[:bx]}, #{Register[:cx]}".asm
-    res << "sete #{Register[:ax].r8}".asm
-  end
-
-  @actions[:!] = proc do |res|
-    res << "mov #{Register[:bx]}, #{Register[:ax]}".asm
-    res << "xor #{Register[:ax]}, #{Register[:ax]}".asm
-    res << "cmp #{Register[:bx]}, 0".asm
-    res << "sete #{Register[:ax].r8}".asm
-  end
-
-  @actions[:*] = proc do |res|
-    res << "pop #{Register[:cx]}".asm
-    res << "imul #{Register[:ax]}, #{Register[:cx]}".asm
-  end
-
-  @actions[:/] = proc do |res|
-    res << "pop #{Register[:cx]}".asm
-    res << "idiv #{Register[:cx]}".asm
-  end
-
-  @actions[:%] = proc do |res|
-    res << "pop #{Register[:cx]}".asm
-    res << "idiv #{Register[:cx]}".asm
-    res << "mov #{Register[:ax]}, #{Register[:dx]}".asm
-  end
-
-  class << self
-    attr_reader :actions
-  end
-
   def initialize(function, *params)
     @function = function
     @parameters = params
-    @action = self.class.actions[function]
+    @action = Action[function]
   end
 
   def code(func_params = [])
