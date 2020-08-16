36e1ba6c8af28662bbd852e39620cf47f3a5d34d
Alastair Gilles <agilles91@gmail.com>
2020-08-16 14:16:25 +0200

    test: add more tests for param-matching

--- spec/compiling_tests/param_matching_spec.rb
+++ spec/compiling_tests/param_matching_spec.rb
@@ -6,6 +6,8 @@ describe 'compiling and running program' do
   let('sample1') { File.expand_path('../fixtures/params_matching1.sag', File.dirname(__FILE__)) }
   let('sample2') { File.expand_path('../fixtures/params_matching2.sag', File.dirname(__FILE__)) }
   let('sample3') { File.expand_path('../fixtures/params_matching3.sag', File.dirname(__FILE__)) }
+  let('sample4') { File.expand_path('../fixtures/params_matching4.sag', File.dirname(__FILE__)) }
+  let('sample5') { File.expand_path('../fixtures/params_matching5.sag', File.dirname(__FILE__)) }
   let('out_file') { 'a.out' }
 
   after(:example) do
@@ -46,5 +48,29 @@ describe 'compiling and running program' do
 
     `./#{out_file}`
     expect($CHILD_STATUS.exitstatus).to eq 28
+  end
+
+  it 'should return 24 for params_matching4.sag.sag' do
+    o, e, s = Open3.capture3("#{slwslvr} #{sample4}")
+
+    puts e if s.exitstatus != 0
+    expect(s.exitstatus).to eq 0
+    expect(e).to be_empty
+    expect(o).to be_empty
+
+    `./#{out_file}`
+    expect($CHILD_STATUS.exitstatus).to eq 24
+  end
+
+  it 'should return 1 for params_matching5.sag.sag' do
+    o, e, s = Open3.capture3("#{slwslvr} #{sample5}")
+
+    puts e if s.exitstatus != 0
+    expect(s.exitstatus).to eq 0
+    expect(e).to be_empty
+    expect(o).to be_empty
+
+    `./#{out_file}`
+    expect($CHILD_STATUS.exitstatus).to eq 1
   end
 end
