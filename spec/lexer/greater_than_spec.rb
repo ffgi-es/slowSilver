19ad60380142cce7f1b80744f2db0e37c7880084
Alastair Gilles <agilles91@gmail.com>
2020-08-27 19:19:13 +0200

    feat: lex greater than

---
+++ spec/lexer/greater_than_spec.rb
@@ -0,0 +1,21 @@
+require 'lexer'
+require 'token'
+
+describe 'Lexer' do
+  describe '#lex' do
+    it 'should return a list of tokens for greater_than1.sag' do
+      in_file = File.expand_path '../fixtures/greater_than1.sag', File.dirname(__FILE__)
+      out_tokens = [
+        Token.new(:type, :INT),
+        Token.new(:identifier, 'main'),
+        Token.new(:return),
+        Token.new(:integer_constant, 4),
+        Token.new(:function_call, '>'),
+        Token.new(:integer_constant, 3),
+        Token.new(:end)
+      ]
+      lexer = Lexer.new(in_file)
+      expect(lexer.lex).to eq out_tokens
+    end
+  end
+end
