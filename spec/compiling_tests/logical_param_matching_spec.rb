1c0315b78c0cc6cd8325bd0214cc49a54e89a9a8
Alastair Gilles <agilles91@gmail.com>
2020-09-05 11:53:02 +0200

    test: logical and normal param matching
    Add tests for a mixture of logical and value matching for parameters on
    separate clauses.

--- spec/compiling_tests/logical_param_matching_spec.rb
+++ spec/compiling_tests/logical_param_matching_spec.rb
@@ -4,7 +4,10 @@ describe 'logial parameter conditions compiler tests' do
   include_context 'compiling test'
 
   [
-    ['logical_param_matching1.sag', 20]
+    ['logical_param_matching1.sag', 20],
+    ['logical_param_matching2.sag', 10],
+    ['logical_param_matching3.sag', 3],
+    ['logical_param_matching4.sag', 1]
   ]
     .each { |(file, exit_code)| include_examples 'exit code', file, exit_code }
 end
