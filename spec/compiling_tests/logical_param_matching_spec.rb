741e6d4789c473e073f23421be928d3bc7e826d5
Alastair Gilles <agilles91@gmail.com>
2020-09-05 12:25:50 +0200

    test: for mixed logical and value matching
    add some tests for a function with value and logical matching in the
    same clause.

--- spec/compiling_tests/logical_param_matching_spec.rb
+++ spec/compiling_tests/logical_param_matching_spec.rb
@@ -7,7 +7,11 @@ describe 'logial parameter conditions compiler tests' do
     ['logical_param_matching1.sag', 20],
     ['logical_param_matching2.sag', 10],
     ['logical_param_matching3.sag', 3],
-    ['logical_param_matching4.sag', 1]
+    ['logical_param_matching4.sag', 1],
+    ['logical_param_matching5.sag', 8],
+    ['logical_param_matching6.sag', 2],
+    ['logical_param_matching7.sag', 5],
+    ['logical_param_matching8.sag', 14]
   ]
     .each { |(file, exit_code)| include_examples 'exit code', file, exit_code }
 end
